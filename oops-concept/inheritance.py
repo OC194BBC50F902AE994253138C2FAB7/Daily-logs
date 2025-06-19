50b5a5f55f7a8584e97f415371fedde8a0e649bf
Aruna <arathanakbca@gmail.com>
2025-06-19 14:57:26 +0530

    oops

--- oops-concept/inheritance.py
+++ oops-concept/inheritance.py
@@ -1,15 +1,59 @@
-# single inheritance
+# # single inheritance
 
-# Base class
-class Parent:
-    def func1(self):
-        print("PARENT CLASS.")
+# # Base class
+# class Parent:
+#     def func1(self):
+#         print("PARENT CLASS.")
 
-# Derived class
-class Child(Parent):
-    def func2(self):
-        print("CHILD CLASS")
+# # Derived class
+# class Child(Parent):
+#     def func2(self):
+#         print("CHILD CLASS")
 
-object = Child()
-object.func1()
-object.func2()
+# object = Child()
+# object.func1()
+# object.func2()
+
+#Multiple inheritance
+
+class Mother :
+    def mother(self, mothername):
+        print(self.mothername)
+
+
+class Father:
+    def father(self,fathername):
+        print(self.fathername)
+
+class Child(Mother, Father):
+    def parents(self):
+        print("MOTHER:",self.mothername)
+        print("FATHER:",self.fathername)
+    
+a = Child()
+a.fathername = "k"
+a.mothername ="A"
+a.parents()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
